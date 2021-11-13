fd05c18d473712b37b698b1720a1bff2dad04709
GiulioZani <giulio.zani@gmail.com>
2021-11-13 10:56:35 +0100

    readme

--- GAT3D/GATMultiHead3D.py
+++ GAT3D/GATMultiHead3D.py
@@ -7,17 +7,25 @@ from GATLayerTemporal import GATLayerTemporal
 class GATMultiHead3D(nn.Module):
     def __init__(self, nfeat, nhid, alpha, nheads, type_):
         super().__init__()
-        if type_ == 'spatial':
-            self.attentions = [GATLayerSpatial(
-                in_features=nfeat, out_features=nhid, alpha=alpha) for _ in range(nheads)]
-        elif type_ == 'temporal':
-            self.attentions = [GATLayerTemporal(
-                in_features=nfeat, out_features=nhid, alpha=alpha) for _ in range(nheads)]
+        if type_ == "spatial":
+            self.attentions = [
+                GATLayerSpatial(
+                    in_features=nfeat, out_features=nhid, alpha=alpha
+                )
+                for _ in range(nheads)
+            ]
+        elif type_ == "temporal":
+            self.attentions = [
+                GATLayerTemporal(
+                    in_features=nfeat, out_features=nhid, alpha=alpha
+                )
+                for _ in range(nheads)
+            ]
         else:
-            raise Exception('Type needs to be one of (spatial|temporal)')
+            raise Exception("Type needs to be one of (spatial|temporal)")
 
         for i, attention in enumerate(self.attentions):
-            self.add_module('attention_{}'.format(i), attention)
+            self.add_module("attention_{}".format(i), attention)
 
     def forward(self, x):
         # N, H, W, T, V = x.size()
