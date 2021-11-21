9d52b6451f332b2228b84f083a6213ecdca6cdb3
caioguirado <caioguirado@yahoo.com.br>
2021-11-21 23:13:13 +0100

    Add experiments 3 and 4

--- train.py
+++ train.py
@@ -165,6 +165,7 @@ def train(
             current_time_step, next_time_step = fix_sizes(
                 current_time_step, next_time_step
             )
+            current_time_step, next_time_step = current_time_step.to(device), next_time_step.to(device)
             optimizer.zero_grad()
             predicted_next_time_step = model(
                 current_time_step
