200641c03b848bf644fa77adc423be68eec51364
GiulioZani <giulio.zani@gmail.com>
2022-01-15 10:26:14 +0100

    experiments

---
+++ experiments/experiment_local_baseline2d/config.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+from convolutional_gat.model import (
+    TemporalModel,
+    SpatialModel,
+    BaselineModel,
+    BaselineModel2D,
+)
+
+MODEL = BaselineModel2D
+PREPROCESSED_FOLDER = "/mnt/kmni_dataset/20_preprocessed"
+MAPPING_TYPE = "linear"
+DATASET = "kmni"
+EPOCHS = 20
+TRAIN_BATCH_SIZE = 16
+TEST_BATCH_SIZE = 16
+LEARNING_RATE = 0.001
+LR_STEP = 1
+GAMMA = 0.95
+PLOT = False
+CRITERION = nn.MSELoss()
+OPTIMIZER = torch.optim.Adam
+DOWNSAMPLE_SIZE = (20, 20)
