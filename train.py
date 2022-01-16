6c36f8105e2242506b138d8ccf8732bbfe32be42
caioguirado <caioguirado@yahoo.com.br>
2022-01-16 21:44:48 +0100

    Merge commit

--- train.py
+++ train.py
@@ -9,12 +9,8 @@ from argparse import ArgumentParser
 from .data_loaders.get_loaders import get_loaders
 from .model import SpatialModel, TemporalModel
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
-from .utils import thresholded_mask_metrics, MSE_denormalized
-=======
 from .unet_model import UnetModel
 from .utils import thresholded_mask_metrics, update_history
->>>>>>> d6f12686d59b3b47b2389106576981d0f9e4a8e1
 
 # todo: add that it saves the best performing model
 
@@ -41,11 +37,6 @@ def plot_history(
     plt.close()
 
 
-<<<<<<< HEAD
-def test(model: nn.Module, device, val_test_loader, label="val", binarize_thresh=0):
-    thresh_metrics = thresholded_mask_metrics(threshold=binarize_thresh, var=val_test_loader.normalizing_var, mean=val_test_loader.normalizing_mean)
-    # denorm_mse = MSE_denormalized(val_test_loader)
-=======
 def accuracy(y, y_hat, mean):
     y = t.clone(y.cpu())
     y_hat = t.clone(y_hat.cpu())
@@ -65,7 +56,6 @@ def test(model: nn.Module, device, val_test_loader, flag="val"):
         var=t.mean(val_test_loader.normalizing_var),
         mean=t.mean(val_test_loader.normalizing_mean),
     )
->>>>>>> d6f12686d59b3b47b2389106576981d0f9e4a8e1
     model.eval()  # We put the model in eval mode: this disables dropout for example (which we didn't use)
     with t.no_grad():  # Disables the autograd engine
         running_loss = t.tensor(0.0)
@@ -82,21 +72,6 @@ def test(model: nn.Module, device, val_test_loader, flag="val"):
                     / t.prod(t.tensor(y.shape[1:]).to(device))
                 ).cpu()
                 total_length += len(x)
-<<<<<<< HEAD
-                
-                running_acc += thresh_metrics.acc(y, y_hat)
-                running_prec += thresh_metrics.precision(y, y_hat)
-                running_recall += thresh_metrics.recall(y, y_hat)
-                # running_denorm_mse += denorm_mse.mse_denormalized_per_pixel(y, y_hat)
-
-    model.train()
-    return (running_loss / total_length).item(), \
-            (running_acc.numpy() / total_length).item(), \
-            (running_prec.numpy() / total_length).item(), \
-            (running_recall.numpy() / total_length).item() \
-            # (running_denorm_mse / total_length).item()
-            
-=======
                 running_acc += accuracy(y, y_hat, 0.04011)
                 # running_acc += thresh_metrics.acc(y, y_hat).numpy()
                 running_prec += thresh_metrics.precision(
@@ -113,7 +88,6 @@ def test(model: nn.Module, device, val_test_loader, flag="val"):
         "val_prec": (running_prec / total_length).item(),
         "val_rec": (running_recall / total_length).item(),
     }
->>>>>>> d6f12686d59b3b47b2389106576981d0f9e4a8e1
 
 
 def visualize_predictions(
@@ -233,27 +207,11 @@ def train_single_epoch(
     scheduler.step()
     train_loss = (running_loss / total_length).item()
     print(f"Train loss: {round(train_loss, 6)}")
-<<<<<<< HEAD
-    # val_loss, val_acc, val_prec, val_rec, val_denorm_mse = test(model, device, val_loader, binarize_thresh)
-    val_loss, val_acc, val_prec, val_rec = test(model, device, val_loader, binarize_thresh)
-    # ipdb.set_trace()
-    print(f"Val loss: {round(val_loss, 6)}")
-    history["train_loss"].append(train_loss)
-    history["val_loss"].append(val_loss)
-
-    # history["denorm_mse"].append(denorm_mse)
-    history["val_acc"].append(val_acc)
-    history["val_prec"].append(val_prec)
-    history["val_rec"].append(val_rec)
-    # history["val_denorm_mse"].append(val_denorm_mse)
-
-=======
     history["train_loss"].append(train_loss)
     test_result = test(model, device, val_loader)
     # print(f"Val loss: {round(test_result['val_loss'], 6)}")
     print(json.dumps(test_result, indent=4))
     update_history(history, test_result)
->>>>>>> d6f12686d59b3b47b2389106576981d0f9e4a8e1
     with open(output_path + "/history.json", "w") as f:
         json.dump(history, f, indent=4)
     if len(history["val_loss"]) > 1 and test_result["val_loss"] < min(
@@ -285,21 +243,8 @@ def train(
     dataset="kmni",
     test_first=True,
 ):
-<<<<<<< HEAD
-    device = t.device(
-        "cuda" if t.cuda.is_available() else "cpu"
-    )  # Select the GPU device, if there is one available.
-    #
-    # device = t.device('cpu')
-    # optimizer = the procedure for updating the weights of our neural network
-    # optimizer = t.optim.Adam(model.parameters(), lr=lr)
-    # criterion = nn.MSELoss()
-    # criterion = nn.BCELoss()  tested but didn't improve significantly
-    history = {"train_loss": [], "val_loss": [], "val_acc": [], "val_prec": [], "val_rec": [], "val_denorm_mse": []}
-=======
     device = t.device("cuda" if t.cuda.is_available() else "cpu")
     history = {"train_loss": []}
->>>>>>> d6f12686d59b3b47b2389106576981d0f9e4a8e1
     print(f"Using device: {device}")
     merge_nodes = model_class == UnetModel
     train_loader, val_loader, test_loader = get_loaders(
@@ -334,24 +279,12 @@ def train(
         optimizer, step_size=lr_step, gamma=gamma
     )
     if test_first:
-<<<<<<< HEAD
-        # test_loss, test_acc, test_prec, test_rec, test_denorm_mse = test(model, device, test_loader, "test", binarize_thresh)
-        test_loss, test_acc, test_prec, test_rec = test(model, device, test_loader, "test", binarize_thresh)
-        print(f"Test loss (without any training): {test_loss}")
-        history["val_loss"].append(test_loss)
-
-        # train_loss, train_acc, train_prec, train_rec, train_denorm_mse = test(model, device, train_loader, "test", binarize_thresh)
-        train_loss, train_acc, train_prec, train_rec = test(model, device, train_loader, "test", binarize_thresh)
-        print(f"Train loss (without any training): {train_loss}")
-        history["train_loss"].append(train_loss)
-=======
         result = test(model, device, train_loader,)
         history["train_loss"].append(result["val_loss"])
         result = test(model, device, test_loader,)
         print(f"Test loss (without any training): {result['val_loss']:.6f}")
         update_history(history, result)
         print(json.dumps(result, indent=4))
->>>>>>> d6f12686d59b3b47b2389106576981d0f9e4a8e1
 
     for epoch in range(1, epochs + 1):
         train_single_epoch(
@@ -383,14 +316,8 @@ def train(
             save=True,
             filename=os.path.join(output_path, f"history_{epoch}.png"),
         )
-<<<<<<< HEAD
-    # test_loss, test_acc, test_prec, test_rec, test_denorm_mse = test(model, device, test_loader, "test", binarize_thresh)
-    test_loss, test_acc, test_prec, test_rec = test(model, device, test_loader, "test", binarize_thresh)
-    print(f"Test loss: {round(test_loss, 6)}")
-=======
     # test_loss = test(model, device, test_loader, "test")
     # print(f"Test loss: {round(test_loss['val_loss'], 6)}")
->>>>>>> d6f12686d59b3b47b2389106576981d0f9e4a8e1
 
     # if plot:
     #   plot_history(history)
